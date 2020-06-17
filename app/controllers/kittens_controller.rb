e59093cf5f262a875047200ffcc70cbaac387992
Shabab Ali <shababsaifi@gmail.com>
2020-06-17 20:22:18 +0530

    Added kitten model and controller

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,25 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+  def new
+    @kitten =Kitten.new
+  end
+  def create
+    @kitten = Kitten.new(kitten_params)
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :softness, :cuteness)
+  end
+end
